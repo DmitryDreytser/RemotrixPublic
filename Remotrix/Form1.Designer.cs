8ce6cd034240db43e434119d27544cf715af0539
ddreitser@stc-spb.ru <ddreitser@stc-spb.ru>
2019-11-13 17:45:59 +0300

    Добавьте файлы проекта.

---
+++ Remotrix/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Remotrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.Results = new System.Windows.Forms.DataGridView();
+            this.QueryBox = new System.Windows.Forms.RichTextBox();
+            this.baseAddress = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Results)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 73);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(255, 747);
+            this.treeView1.TabIndex = 0;
+            // 
+            // Results
+            // 
+            this.Results.AllowUserToAddRows = false;
+            this.Results.AllowUserToDeleteRows = false;
+            this.Results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Results.Location = new System.Drawing.Point(268, 471);
+            this.Results.Name = "Results";
+            this.Results.ReadOnly = true;
+            this.Results.Size = new System.Drawing.Size(1061, 348);
+            this.Results.TabIndex = 1;
+            // 
+            // QueryBox
+            // 
+            this.QueryBox.AcceptsTab = true;
+            this.QueryBox.Location = new System.Drawing.Point(269, 73);
+            this.QueryBox.Name = "QueryBox";
+            this.QueryBox.Size = new System.Drawing.Size(1060, 392);
+            this.QueryBox.TabIndex = 2;
+            this.QueryBox.Text = "";
+            // 
+            // baseAddress
+            // 
+            this.baseAddress.Location = new System.Drawing.Point(12, 12);
+            this.baseAddress.Name = "baseAddress";
+            this.baseAddress.Size = new System.Drawing.Size(692, 20);
+            this.baseAddress.TabIndex = 3;
+            this.baseAddress.Text = "https://napopravku.ru";
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(714, 13);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(346, 20);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "o.dreytser@napopravku.ru";
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(714, 39);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(346, 20);
+            this.Password.TabIndex = 4;
+            this.Password.Text = "Napopravku.ru123";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(155, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1341, 832);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.baseAddress);
+            this.Controls.Add(this.QueryBox);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Results)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView Results;
+        private System.Windows.Forms.RichTextBox QueryBox;
+        private System.Windows.Forms.TextBox baseAddress;
+        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Remotrix/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AngleSharp;
+using AngleSharp.Html.Dom;
+
+namespace Remotrix
+{
+    public partial class Form1 : Form
+    {
+        HttpClient connection = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            connection.BaseAddress = new Uri(this.baseAddress.Text);
+            var result = await connection.GetAsync("/bitrix/admin/");
+            var resp = await result.Content.ReadAsStringAsync();
+            string SessionID;
+
+            using (var context = (BrowsingContext)BrowsingContext.New(Configuration.Default))
+            {
+                using (var document = await context.OpenAsync(req => req.Content(resp)))
+                {
+                    document.Domain = this.baseAddress.Text;
+                    SessionID = ((IHtmlInputElement)document.QuerySelector("input#sessid")).Value;
+                }
+            }
+
+            var auth = new Dictionary<string, string>
+            {
+                {"AUTH_FORM", "Y"},
+                {"Login", "yes"},
+                {"TYPE", "AUTH"},
+                {"USER_LOGIN", Login.Text},
+                {"USER_PASSWORD", Password.Text},
+                {"captcha_sid", ""},
+                {"captcha_word", ""},
+                {"sessid", SessionID},
+            };
+
+            result = await connection.PostAsync("/bitrix/admin/?login=yes", new FormUrlEncodedContent(auth));
+            resp = await result.Content.ReadAsStringAsync();
+            
+                
+            result = await connection.PostAsync("/bitrix/admin/sql.php?PAGEN_1=1&SHOWALL_1=1&lang=ru&mode=frame&table_id=tbl_sql", 
+                new FormUrlEncodedContent(new Dictionary<string, string> 
+                {
+                    {"query", "SELECT * FROM INFORMATION_SCHEMA.TABLES"},
+                    {"sessid", SessionID}
+                } ));
+
+            resp = await result.Content.ReadAsStringAsync();
+
+            using (var context = (BrowsingContext)BrowsingContext.New(Configuration.Default))
+            {
+                using (var document = await context.OpenAsync(req => req.Content(resp)))
+                {
+                    document.Domain = this.baseAddress.Text;
+                    var t = ((IHtmlTableElement)document.QuerySelector("table#tbl_sql"));
+
+                    Results.DataSource = t.Rows.ToList();
+                }
+            }
+
+            QueryBox.Text = resp;
+
+
+
+        }
+    }
+}
